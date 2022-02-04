86d110c9a7634d489d219cdad45b070e3e0c469b
m1ma0314 <m1ma@ucsd.edu>
2022-02-03 23:56:15 -0800

    lib + fail test added

---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    //would copy part of the code from Week 4 Lab WriteUp
+    @Test
+    public void multiplication() {
+        assertEquals(2, SkillDemo.multiply(1, 1));
+    }
+
+    
+}
