c6786aa7ea81e15b2bc945f7a354cd5ca3eef43e
m1ma0314 <m1ma@ucsd.edu>
2022-02-03 23:59:11 -0800

    error fixed

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -5,7 +5,7 @@ public class SkillDemoTest {
     //would copy part of the code from Week 4 Lab WriteUp
     @Test
     public void multiplication() {
-        assertEquals(2, SkillDemo.multiply(1, 1)); //obviously it would fail
+        assertEquals(2, SkillDemo.multiply(2, 1)); //place where it fails
     }
 
     
